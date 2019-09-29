9350a410abdfb1dab73c7eed40297b2e2e628033
Joshua Reed <jreed1701@gmail.com>
2019-09-29 16:57:24 -0400

    Add bootstrap and simple landing page.

--- app/api/views.py
+++ app/api/views.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint
+from flask import Blueprint, render_template
+from .content import Content
 
-api = Blueprint('app', __name__)
+api = Blueprint('app', __name__, template_folder='../templates', static_folder='../static')
 
-@api.route('/hello')
-def hello():
-    print('something happened')
-    return 'Hello World'
+TOP_DICT = Content()
 
+@api.route('/')
+def main():
+    return render_template('main.html')
 
-
+@api.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
