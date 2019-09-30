0f97d229b6cef95cf8ebd8c4e39fe6696d3ae2fd
Joshua Reed <jreed1701@gmail.com>
2019-09-29 20:46:41 -0400

    Set up bootstrap starter template as index page.

--- app/api/views.py
+++ app/api/views.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template
-from .content import Content
 
-api = Blueprint('app', __name__, template_folder='../templates', static_folder='../static')
-
-TOP_DICT = Content()
+api = Blueprint('app', 
+                __name__, 
+                template_folder='../templates', 
+                static_folder='../static', 
+                static_url_path='/app/static')
 
 @api.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('index.html')
 
 @api.errorhandler(404)
 def page_not_found(e):
